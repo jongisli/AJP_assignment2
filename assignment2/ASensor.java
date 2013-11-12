10a521c332b703d2e23f7110dd5bc07e6463fe4a
BenBenZou <zoubin1401@gmail.com>
2013-11-12 14:32:45 +0100

    shit

--- assignment2/ASensor.java
+++ assignment2/ASensor.java
@@ -1,12 +1,22 @@
 package assignment2;
 
 import java.util.List;
+import java.util.Random;
 
 public class ASensor implements Sensor, Runnable {
 	List<Monitor> monitors; //A sensor can push readings to one or many monitors
+	SensorReading sr;
 	
 	public SensorReading generateSensorReading() {
-		// TODO Auto-generated method stub
+		
+		Random rand = new Random();
+
+	    float randomNumTem = rand.nextInt((80 - 10) + 1) + 10;
+	    float randomNumHum = rand.nextInt((95 - 50) + 1) + 50;
+
+		sr.setHumidity(randomNumHum);
+		sr.setTemperature(randomNumTem);
+		
 		return null;
 	}
 
@@ -24,7 +34,10 @@ public class ASensor implements Sensor, Runnable {
 
 	@Override
 	public int registerMonitor(List<Monitor> sm) {
-		// TODO Auto-generated method stub
+		for(int i=0; i < sm.size(); i++){
+			monitors.add(sm.get(i));
+		}
+			
 		return 0;
 	}
 
